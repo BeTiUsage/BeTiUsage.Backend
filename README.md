46447783ed34e882e0c068617b10fd1dc3e18cef
DragonSlayer <varattleff@Viktors-MacBook-Air.local>
2025-04-23 14:11:32 +0200

    basic structure done

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # BeTiUsage
+
+## Better Time Usage
+
+### Segments
+
+- [Installation](#installation)
+- [Running the application](#running-the-application)
+
+### Installation
+
+1. Clone the repository
+   ```bash
+   git clone https://github.com/BeTiUsage/BeTiUsage.Backend
+   ```
+
+2. Enter the directory
+   ```bash
+   cd  BiTiUsage
+   ```
+
+### Running the application
+
+1. Set up environment variables
+2. Run the Application
+
+[Now the backend should be running on `http://localhost:8080/`](http://localhost:8080/)
+
+
+
---
+++ src/main/java/test/betiusage/configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package test.betiusage.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     * This method is used to configure the CORS settings for the application.
+     * It allows requests from the specified origin, methods, headers and credentials.
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
