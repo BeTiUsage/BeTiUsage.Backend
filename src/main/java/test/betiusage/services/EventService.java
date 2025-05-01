e572d8da733973f76a8a2abfe5d6d65088fdeb3d
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2025-05-01 19:28:54 +0200

    fixed small error

--- src/main/java/test/betiusage/services/EventService.java
+++ src/main/java/test/betiusage/services/EventService.java
@@ -23,7 +23,7 @@ public class EventService {
         EventDTO eventDTO = new EventDTO();
         eventDTO.setId(event.getId());
         eventDTO.setHobbyName(event.getHobby().getName());
-        eventDTO.setCategoryName(event.getHobby().getCategories().get(0).getName());
+        eventDTO.setCategoryName(event.getCategory().getName());
         eventDTO.setName(event.getName());
         eventDTO.setDescription(event.getDescription());
         eventDTO.setLocation(event.getLocation());
