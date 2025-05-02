6c219f411f7aab3491556e4e822c67198b614692
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2025-05-02 19:54:19 +0200

    @repository added

--- src/main/java/test/betiusage/repositorys/EventRepository.java
+++ src/main/java/test/betiusage/repositorys/EventRepository.java
@@ -1,7 +1,9 @@
 package test.betiusage.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import test.betiusage.entitys.Event;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
