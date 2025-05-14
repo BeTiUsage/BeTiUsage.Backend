e8b08d4cdef91923613989728c5aaabbd1aa7814
DragonSlayer <viktoraugustrattleff@gmail.com>
2025-05-14 11:03:26 +0200

    update cors error

--- src/main/java/home/betiusage/configuration/CorsConfig.java
+++ src/main/java/home/betiusage/configuration/CorsConfig.java
@@ -1,31 +1,41 @@
 package home.betiusage.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    /**
-     * This method is used to configure the CORS settings for the application.
-     * It allows requests from the specified origin, methods, headers and credentials.
-     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/**")
-            .allowedOrigins("http://localhost:4242", "https://better-time-usage-2ph6x.ondigitalocean.app", "https://better-time-usage.vercel.app")
-            .allowedMethods(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "PATCH",
-                "HEAD",
-                "OPTIONS"
-            )
-            .allowedHeaders("*")
-            .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4242", "https://better-time-usage-2ph6x.ondigitalocean.app", "https://better-time-usage.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4242");
+        config.addAllowedOrigin("https://better-time-usage-2ph6x.ondigitalocean.app");
+        config.addAllowedOrigin("https://better-time-usage.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
