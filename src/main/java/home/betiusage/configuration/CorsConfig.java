cb38826e6e9a55cb4ef59f8fb52e786856c78beb
DragonSlayer <viktoraugustrattleff@gmail.com>
2025-05-14 11:18:53 +0200

    trying to fix cors issue test 7

--- src/main/java/home/betiusage/configuration/CorsConfig.java
+++ src/main/java/home/betiusage/configuration/CorsConfig.java
@@ -5,35 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4242", "https://better-time-usage-2ph6x.ondigitalocean.app", "https://better-time-usage.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4242");
-        config.addAllowedOrigin("https://better-time-usage-2ph6x.ondigitalocean.app");
-        config.addAllowedOrigin("https://better-time-usage.vercel.app");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        config.setMaxAge(3600L);
+        config.setAllowCredentials(true);
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
