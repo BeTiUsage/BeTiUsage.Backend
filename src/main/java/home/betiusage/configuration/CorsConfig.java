b14ba9e514aae01f8f2a3d29c020d4acea761c17
DragonSlayer <viktoraugustrattleff@gmail.com>
2025-05-14 11:45:44 +0200

    here we go again

--- src/main/java/home/betiusage/configuration/CorsConfig.java
+++ src/main/java/home/betiusage/configuration/CorsConfig.java
@@ -1,24 +1,26 @@
 package home.betiusage.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.setAllowCredentials(true);
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:4242", "https://better-time-usage-2ph6x.ondigitalocean.app", "https://better-time-usage.vercel.app")
+            .allowedMethods(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "PATCH",
+                "HEAD",
+                "OPTIONS"
+            )
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
